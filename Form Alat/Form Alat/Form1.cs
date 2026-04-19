83efa6120708a6aaa5055c7944ec8d044924e3ea
dev0062 <zaindevalldino@gmail.com>
2026-04-19 16:27:08 +0700

    Membuat  void untuk button TambahAlat

--- Form Alat/Form Alat/Form1.cs
+++ Form Alat/Form Alat/Form1.cs
@@ -35,7 +35,7 @@ namespace Form_Alat
 
                 dataGridView1.Columns.Add("Nama_Alat", "Nama Alat");
                 dataGridView1.Columns.Add("Stok", "Stok");
-
+                
 
                 string query = "SELECT * FROM Alat";
 
@@ -47,7 +47,7 @@ namespace Form_Alat
                     dataGridView1.Rows.Add(
                         reader["Nama_Alat"].ToString(),
                         reader["stok"].ToString()
-
+                        
                      );
                 }
                 reader.Close();
@@ -56,9 +56,60 @@ namespace Form_Alat
             {
                 MessageBox.Show("Gagal menampilkan data: " + ex.Message);
             }
-
+            
         }
 
+        private void btnTambahAlat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
 
+                if (TXTNamaAlat.Text == "")
+                {
+                    MessageBox.Show("Nama Alat Wajib Diisyi");
+                    TXTNamaAlat.Focus();
+                    return;
+                }
+
+                if (TXTStok.Text == "")
+                {
+                    MessageBox.Show("Stok Wajib Diisyi");
+                    TXTStok.Focus();
+                    return;
+                }
+
+                string query = @"INSERT INTO Alat
+                                (Nama_Alat, Stok) VALUES (@Nama_Alat, @Stok)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@Nama_Alat", TXTNamaAlat.Text);
+                cmd.Parameters.AddWithValue("@Stok", TXTStok.Text);
+                
+
+                int result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Data Alat berhasil ditambahkyan");
+                    ClearForm();
+                    btnMenampilkanDataAlat.PerformClick();
+                }
+                else
+                {
+                    MessageBox.Show("Data gagal ditambahkyan");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesyalahan: " + ex.Message);
+            }
+        }
+
+        
     }
+    
 }
