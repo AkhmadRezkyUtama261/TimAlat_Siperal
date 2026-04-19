c3d373d160073bbe0765ce266b4673cf7bcbae94
dev0062 <zaindevalldino@gmail.com>
2026-04-19 16:23:57 +0700

    Membuat Void untuk button MenampilkanDataAlat

---
+++ Form Alat/Form Alat/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Form_Alat
+{
+    public partial class Form1 : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=DEVALLDINOZAIN\\DEV;Initial Catalog= DBPeminjamanAlat;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void btnMenampilkanDataAlat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+
+                dataGridView1.Columns.Add("Nama_Alat", "Nama Alat");
+                dataGridView1.Columns.Add("Stok", "Stok");
+
+
+                string query = "SELECT * FROM Alat";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(
+                        reader["Nama_Alat"].ToString(),
+                        reader["stok"].ToString()
+
+                     );
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menampilkan data: " + ex.Message);
+            }
+
+        }
+
+
+    }
+}
