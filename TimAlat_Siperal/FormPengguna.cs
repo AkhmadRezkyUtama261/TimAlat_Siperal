a4c0185199a5c3e48a980cf4277c70b4dd814bce
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2026-04-20 04:27:27 +0700

    fisrt commit

---
+++ TimAlat_Siperal/FormPengguna.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace TimAlat_Siperal
+{
+    public partial class FormPengguna : Form
+    {
+        Koneksi konn = new Koneksi();
+        SqlDataAdapter da;
+        DataTable dt;
+        SqlCommand cmd;
+
+       
+        string nikLama = "";
+
+        public FormPengguna()
+        {
+            InitializeComponent();
+            TampilData();
+        }
+
+        void TampilData()
+        {
+            using (SqlConnection conn = konn.GetConn())
+            {
+                try
+                {
+                    da = new SqlDataAdapter("SELECT * FROM Peminjam", conn);
+                    dt = new DataTable();
+                    da.Fill(dt);
+                    dgvPengguna.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error Tampil Data: " + ex.Message);
+                }
+            }
+        }
+
+        void Bersihkan()
+        {
+            txtNIK.Clear();
+            txtNama.Clear();
+            txtAlamat.Clear();
+            txtTelp.Clear();
+
+            nikLama = ""; 
+            txtNIK.Enabled = true; 
+            txtNIK.Focus();
+        }
+
+       
---
+++ TimAlat_Siperal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimAlat_Siperal
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // UBAH BARIS DI BAWAH INI:
+            // Dari Form1() menjadi FormPengguna()
+            Application.Run(new FormPengguna());
+        }
+    }
+}
