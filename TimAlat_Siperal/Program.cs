e0358ca9e3b61f9c4f8b0dedcb153d238bf9192e
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2026-04-20 07:28:42 +0700

    progam.cs form pengguna rev

--- TimAlat_Siperal/Program.cs
+++ TimAlat_Siperal/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TimAlat_Siperal
@@ -9,7 +6,7 @@ namespace TimAlat_Siperal
     internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Pintu masuk utama aplikasi.
         /// </summary>
         [STAThread]
         static void Main()
@@ -17,11 +14,18 @@ namespace TimAlat_Siperal
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Mengarahkan aplikasi untuk membuka Form Peminjaman saat pertama kali dijalankan
-            Application.Run(new FormPeminjaman());
+            // ===================================================================
+            // CARA PAKAI: 
+            // Pastikan hanya ada SATU baris "Application.Run" yang tidak berwarna hijau.
+            // ===================================================================
+
+            // 1. Aktifkan baris ini kalau mau buka Form Peminjaman (Transaksi)
+            //Application.Run(new FormPeminjaman());
+
+            // 2. Aktifkan baris ini kalau mau buka Form Pengguna (Data Member)
+            Application.Run(new FormPengguna());
+
+            
         }
     }
 }
-
-
-
