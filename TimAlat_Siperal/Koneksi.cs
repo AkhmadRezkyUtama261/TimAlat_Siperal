d6c400916ccdd092f1fccb4ec5a06b9ff7becf48
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2026-06-23 04:27:09 +0700

    final

--- TimAlat_Siperal/Koneksi.cs
+++ TimAlat_Siperal/Koneksi.cs
@@ -5,11 +5,13 @@ namespace TimAlat_Siperal
 {
     class Koneksi
     {
-  
         public SqlConnection GetConn()
         {
             SqlConnection conn = new SqlConnection();
-            conn.ConnectionString = @"Data Source=LAPTOP-7SOCNODM\ANDHIKA1;Initial Catalog=DBPeminjamanAlat;Integrated Security=True";
+
+            // Menggunakan IP Address (192.168.94.188) agar bisa diakses Client
+            conn.ConnectionString = @"Data Source=192.168.94.188\ANDHIKA1;Initial Catalog=DBPeminjamanAlat;User ID=sa;Password=Purworejo123;Encrypt=False";
+
             return conn;
         }
     }
